be5140678b5271a1f37efa8fc36e4a254cf052a2
Giannis Daras <daras.giannhs@gmail.com>
2018-05-23 21:47:13 +0300

    Init file fixes. Now, Greek language can be loaded normally

--- spacy/lang/el/__init__.py
+++ spacy/lang/el/__init__.py
@@ -2,7 +2,11 @@ from __future__ import unicode_literals
 from .stop_words import STOP_WORDS
 from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .lex_attrs import LEX_ATTRS
-
+from .tag_map import TAG_MAP
+from .lemmatizer import LOOKUP
+from .morph_rules import MORPH_RULES
+from .syntax_iterators import SYNTAX_ITERATORS
+# Default imports
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
 from ...language import Language
 from ...attrs import LANG
@@ -10,10 +14,14 @@ from ...util import update_exc
 
 class GreekDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
-    lex_attr_getters[LANG] = lambda text: 'el'
     lex_attr_getters.update(LEX_ATTRS)
+    lex_attr_getters[LANG] = lambda text: 'el'
     tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
+    tag_map= TAG_MAP
     stop_words = STOP_WORDS
+    lemma_lookup= LOOKUP
+    morph_rules = MORPH_RULES
+    syntax_iterators = SYNTAX_ITERATORS
 
 class Greek(Language):
     lang = 'el' 
