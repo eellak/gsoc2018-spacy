ac83c338da3825a9db97db73c41b8dd97970bb4a
Giannis Daras <daras.giannhs@gmail.com>
2018-05-23 22:48:19 +0300

    Procedure for fixing stop words from stop_words_finder

---
+++ spacy/lang/el/etc/stop_words_corrector.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+import myfuncs
+all_words=defaultdict(lambda: '')
+with open("greek_dict.in") as fileinst:
+	for line in fileinst:
+		all_words[myfuncs.strip_accents(line.rstrip())]=line.rstrip()
+out=defaultdict(lambda:'')
+with open("stop_words_without_accents.out") as fileinst:
+	for line in fileinst:
+		for x in line.rstrip().split():
+			if (all_words[x]!=''):
+				out[x]=all_words[x]
+
+previous='α'
+# sorts strings after removing accents
+for i in sorted(out.keys()):
+	# new line when letter changes
+	if (out[i][0]!=previous):
+		previous=out[i][0]
+		print()
+	print("{} ".format(out[i]),end='') 
