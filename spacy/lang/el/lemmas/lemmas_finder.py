48966f614d559cb46263a4bb371038e9a1e06bf2
Giannis Daras <daras.giannhs@gmail.com>
2018-05-28 20:34:41 +0300

    Lemmas pairing based on existing lemmas and given words in our dictionary

--- spacy/lang/el/lemmas/lemmas_finder.py
+++ spacy/lang/el/lemmas/lemmas_finder.py
@@ -1,19 +1,50 @@
 import stemmer
+import sys
+sys.path.insert(0,"../etc");
 import myfuncs
 from collections import defaultdict
 stemms=defaultdict(list)
 input_words=[]
-with open("greek_dict.in") as fileinst:
+lemmas=defaultdict(lambda:'null')
+not_matched=[]
+matched=[]
+recover=defaultdict(lambda:'null')
+with open("../etc/greek_dict.in") as fileinst:
 	for line in fileinst:
-		input_words.append(myfuncs.strip_accents(line.rstrip()).upper())
+		tmp=myfuncs.strip_accents(line.rstrip()).upper()
+		recover[tmp]=line.rstrip()
+		input_words.append(tmp)
+
+with open("merged.out") as fileinst:
+	for line in fileinst:
+		tmp=myfuncs.strip_accents(line.rstrip()).upper()
+		lemmas[tmp]="not null"
+		recover[tmp]=line.rstrip()
+
+
 for i in input_words:
 	stemms[stemmer.stem(i)].append(i)
 
 for key,value in stemms.items():
-	print("{} :".format(key),end='')
+	found=0
 	for j in value:
-		print("{}, ".format(j),end='')
-	print()
+		if (lemmas[j]!="null"):
+			found=j
+			break
+	if (found==0):
+		not_matched.append(key)
+	else:
+		for j in value:
+			if (j!=found):
+				matched.append([recover[j],recover[found]])
 
+with open('matched.txt','a') as fileinst:
+	for x in matched:
+		fileinst.write("\"{}\":\"{}\"".format(x[0],x[1]))
+		fileinst.write("\n")
 
+with open('not_matched.txt','a') as fileinst:
+	for x in not_matched:
+		fileinst.write(x)
+		fileinst.write('\n')
 
