7a01dfc9e0d6317fd8fee446719b7607d4a11245
Giannis Daras <daras.giannhs@gmail.com>
2018-06-07 14:22:42 +0300

    Norm exceptions included in init file

--- spacy/lang/el/__init__.py
+++ spacy/lang/el/__init__.py
@@ -6,8 +6,10 @@ from .tag_map import TAG_MAP
 from .lemmatizer import LOOKUP
 from .morph_rules import MORPH_RULES
 from .syntax_iterators import SYNTAX_ITERATORS
+from .norm_exceptions import NORM_EXCEPTIONS
 # Default imports
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
+from ..norm_exceptions import BASE_NORMS
 from ...language import Language
 from ...attrs import LANG
 from ...util import update_exc
@@ -16,6 +18,8 @@ class GreekDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters.update(LEX_ATTRS)
     lex_attr_getters[LANG] = lambda text: 'el'
+    lex_attr_getters[NORM] = add_lookups(Language.Defaults.lex_attr_getters[NORM],
+                                         BASE_NORMS, NORM_EXCEPTIONS)
     tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
     tag_map= TAG_MAP
     stop_words = STOP_WORDS
