6202b464e0fd74a34b8d82f814f5fb2e237b9bdd
Giannis Daras <daras.giannhs@gmail.com>
2018-06-09 01:34:02 +0300

    Script for producing sentences with proper nouns for later training

---
+++ spacy/lang/el/named_entities/propn_detector.py
@@ -0,0 +1,21 @@
+import spacy
+import logging
+from gensim.corpora import WikiCorpus, MmCorpus
+nlp=spacy.load('el_unnamed')
+
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+wiki = WikiCorpus("../etc/elwiktionary-latest-pages-articles.xml.bz2",lemmatize=False, dictionary={})
+sentences = wiki.get_texts()
+counter=0
+for sentence in sentences:
+	doc=nlp(sentence)
+	flag=False
+	for i in doc:
+		if (i.tag_=='PROPN'):
+			flag=True
+	if (flag==True):
+		counter+=1
+		print(sentence)
+	if (counter==20000):
+		break
