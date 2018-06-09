bbbf4cdbc7db6646640f25dbf1e833471d45254b
Giannis Daras <daras.giannhs@gmail.com>
2018-06-09 16:56:38 +0300

    Pep8 and importing script as module functionality added

--- spacy/lang/el/lemmas/lemmas_neural/lemmatizer_neural.py
+++ spacy/lang/el/lemmas/lemmas_neural/lemmatizer_neural.py
@@ -1,48 +1,43 @@
 import distance
 from gensim.models import FastText
 import sys
-sys.path.insert(0,"../../");
+sys.path.insert(0, "../../")
 from lemmatizer import LOOKUP
 
 # Model wiki.el.vec.bin can be downloaded from:
 # https://github.com/facebookresearch/fastText/blob/master/pretrained-vectors.md
-model=FastText.load_fasttext_format('wiki.el.bin')
+model = FastText.load_fasttext_format('wiki.el.bin')
 
 
 class Relation:
-	def __init__(self,dist,text):
-		self.distance=dist
-		self.word=text
+    def __init__(self, dist, text):
+        self.distance = dist
+        self.word = text
 
 
 def lemmatizer(word):
 
-	try:
-		res=LOOKUP[word]
-		return res
-	except:
-		related_words=model.most_similar(word)
-		objects_seq=[]
-		for i in related_words:
-			dist=distance.levenshtein(word,i)
-			objects_seq.append(Relation(dist,i))
-		objects_seq.sort(key=lambda x: x.distance)
-		for i in objects_seq:
-			try:
-				res=LOOKUP[i.word]
-				return res
-			except:
-				pass
-		return word
-
-def run():
-	while(True):
-		x=input('Give me word to search lemma\n')
-		res=lemmatizer(x)
-		print('Lemma: {}'.format(res))
-
-run()
+    try:
+        res = LOOKUP[word]
+        return res
+    except:
+        related_words = model.most_similar(word)
+        objects_seq = []
+        for i in related_words:
+            dist = distance.levenshtein(word, i)
+            objects_seq.append(Relation(dist, i))
+        objects_seq.sort(key=lambda x: x.distance)
+        for i in objects_seq:
+            try:
+                res = LOOKUP[i.word]
+                return res
+            except:
+                pass
+        return word
 
 
-
-
+if __name__ = "__main__":
+    while(True):
+        x = input('Give me word to search lemma\n')
+        res = lemmatizer(x)
+        print('Lemma: {}'.format(res))
