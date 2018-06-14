8f5374056ad672b4f91dac2e181af2b85ba5265f
Giannis Daras <daras.giannhs@gmail.com>
2018-06-15 00:34:41 +0300

    Add get_accent function
    Function interface: gets string, returns string with accent.

--- spacy/lang/el/etc/myfuncs.py
+++ spacy/lang/el/etc/myfuncs.py
@@ -1,5 +1,18 @@
 import unicodedata
+
 def strip_accents(s):
    s=s.lower()
    return ''.join(c for c in unicodedata.normalize('NFD', s)
                   if unicodedata.category(c) != 'Mn')
+
+ls=defaultdict(lambda: '')
+with open('greek_dict.in') as fileinst:
+        for line in fileinst:
+                tmp_str=line.rstrip()
+                ls[strip_accents(tmp_str)]=tmp_str
+
+def get_accent(word):
+        try:
+                return ls[word]
+        except:
+                return[word]
