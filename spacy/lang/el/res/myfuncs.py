36bd320df6f3921d943f3386fc828d02722fbf07
Giannis Daras <daras.giannhs@gmail.com>
2018-06-19 12:46:53 +0300

    Improve util functions for greek language
    Getting word with accent takes into consideration that some words in the vocab may have or may not have accents.

--- spacy/lang/el/res/myfuncs.py
+++ spacy/lang/el/res/myfuncs.py
@@ -1,18 +1,22 @@
 import unicodedata
+from collections import defaultdict
 
 def strip_accents(s):
    s=s.lower()
    return ''.join(c for c in unicodedata.normalize('NFD', s)
                   if unicodedata.category(c) != 'Mn')
 
-ls=defaultdict(lambda: '')
-with open('greek_dict.in') as fileinst:
+words={}
+with open('dictionaries/greek_dict.in') as fileinst:
         for line in fileinst:
                 tmp_str=line.rstrip()
-                ls[strip_accents(tmp_str)]=tmp_str
+                words[strip_accents(tmp_str)]=tmp_str
 
 def get_accent(word):
         try:
-                return ls[word]
+          if (words[word]==word):
+            return word
+          else:
+            return words[word]
         except:
-                return[word]
+          return word
