a7a27be18528d9d38196461b097524a6f6c559b0
Giannis Daras <daras.giannhs@gmail.com>
2018-06-20 13:08:38 +0300

    Fix file name typo

--- spacy/lang/el/stop-words/stop_words_finder.py
+++ spacy/lang/el/stop-words/stop_words_finder.py
@@ -9,15 +9,16 @@ num_of_words = 350
 count=1
 with open('global-freqs/out.txt') as fileinst:
     for line in fileinst:
-        tmp_line=line.split(' ')
+        tmp_line=line.rsplit()
         count+=1
+        words.append(tmp_line[2].replace('\'',''))
         if (count==350):
             break
     
 
 words=sorted(words)
 previous='α'
-with open('stop_wors.out','w') as fileinst:
+with open('stop_words.out','w') as fileinst:
     for i in sorted(words):
         # new line when letter changes
         if (i[0]!=previous):
