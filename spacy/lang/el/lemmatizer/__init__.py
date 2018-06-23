29dc8fd88746aedcc4996ade8314ebb9eeea9907
Giannis Daras <daras.giannhs@gmail.com>
2018-06-23 13:07:34 +0300

    Add lemmatizer adjectives exceptions

--- spacy/lang/el/lemmatizer/__init__.py
+++ spacy/lang/el/lemmatizer/__init__.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from .lookup import LOOKUP
 from ._adjectives import ADJECTIVES
+from ._adjectives_irreg import ADJECTIVES_IRREG
 from ._adverbs import ADVERBS
 from ._nouns import NOUNS
 from ._verbs import VERBS
@@ -14,3 +15,6 @@ LEMMA_INDEX = {'adj': ADJECTIVES, 'adv': ADVERBS, 'noun': NOUNS, 'verb': VERBS}
 
 LEMMA_RULES = {'adj': ADJECTIVE_RULES, 'noun': NOUN_RULES, 'verb': VERB_RULES,
                'punct': PUNCT_RULES}
+
+
+LEMMA_EXC = {'adj': ADJECTIVES_IRREG}
