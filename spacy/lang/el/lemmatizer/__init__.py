8e15e42d3c919906c1bd61478ecfe795bd54d059
Giannis Daras <daras.giannhs@gmail.com>
2018-07-04 14:04:33 +0300

    add verb exceptions

--- spacy/lang/el/lemmatizer/__init__.py
+++ spacy/lang/el/lemmatizer/__init__.py
@@ -6,6 +6,7 @@ from ._adjectives import ADJECTIVES
 from ._adjectives_irreg import ADJECTIVES_IRREG
 from ._adverbs import ADVERBS
 from ._nouns_irreg import NOUNS_IRREG
+from ._verbs_irreg import VERBS_IRREG
 from ._nouns import NOUNS
 from ._verbs import VERBS
 from ._lemma_rules import ADJECTIVE_RULES, NOUN_RULES, VERB_RULES, PUNCT_RULES
@@ -18,4 +19,4 @@ LEMMA_RULES = {'adj': ADJECTIVE_RULES, 'noun': NOUN_RULES, 'verb': VERB_RULES,
                'punct': PUNCT_RULES}
 
 
-LEMMA_EXC = {'adj': ADJECTIVES_IRREG, 'noun': NOUNS_IRREG}
+LEMMA_EXC = {'adj': ADJECTIVES_IRREG, 'noun': NOUNS_IRREG, 'verb': VERBS_IRREG}
