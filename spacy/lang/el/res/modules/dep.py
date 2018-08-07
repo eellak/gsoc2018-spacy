d559c021141b2fc6763388b49fc2d7490bcfefe5
Giannis Daras <daras.giannhs@gmail.com>
2018-08-07 17:11:52 +0300

    Fix small bug to dep module

--- spacy/lang/el/res/modules/dep.py
+++ spacy/lang/el/res/modules/dep.py
@@ -1,14 +1,14 @@
 import spacy
 from nltk import Tree
 nlp = spacy.load('el')
-doc = nlp("Ο Κώστας αγόρασε πατάτες και τις άφησε πάνω στο ψυγείο.")
+doc = nlp("η δημοκρατία είναι το πιο ανθρώπινο πολίτευμα.")
 
 
 for token in doc:
     print('Token:{}, DEP tag: {}'.format(token,token.dep_))
 def to_nltk_tree(node):
-if node.n_lefts + node.n_rights > 0:
-  return Tree(node.orth_, [to_nltk_tree(child) for child in node.children])
-else:
-  return node.orth_
+    if node.n_lefts + node.n_rights > 0:
+      return Tree(node.orth_, [to_nltk_tree(child) for child in node.children])
+    else:
+      return node.orth_
 [to_nltk_tree(sent.root).pretty_print() for sent in doc.sents]
