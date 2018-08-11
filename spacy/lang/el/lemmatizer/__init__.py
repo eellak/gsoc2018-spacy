c98c037489c20183a2275ccef0e33bec377f9c95
Giannis Daras <daras.giannhs@gmail.com>
2018-08-11 21:35:06 +0300

    Sync lemmatizer with upstream

--- spacy/lang/el/lemmatizer/__init__.py
+++ spacy/lang/el/lemmatizer/__init__.py
@@ -1,7 +1,6 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-from .lookup import LOOKUP
 from ._adjectives import ADJECTIVES
 from ._adjectives_irreg import ADJECTIVES_IRREG
 from ._adverbs import ADVERBS
